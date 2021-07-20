1dd1a6bc6331a8fab6251f11be00f5989eeb4c35
ToruTakahashi5431 <69805601+ToruTakahashi5431@users.noreply.github.com>
2021-07-20 16:07:53 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import "./styles.css";
+
+const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化する
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  // div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // pタグ生成
+  const p = document.createElement("p");
+  p.innerText = inputText;
+
+  // divタグの子要素に各要素を設定
+  div.appendChild(p);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
